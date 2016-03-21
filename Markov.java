3bdf161802841011554895ad391eef985906a0fa
Joseph Burley <jburley3@students.kennesaw.edu>
2016-03-21 12:48:20 -0400

    Improvements to string manipulation.
    No longer considers commas, periods, or case.

--- Markov.java
+++ Markov.java
@@ -4,20 +4,25 @@ import java.lang.Math;
 import java.io.*;
 public class Markov
 {
-   private class Bucket
+   private static class Bucket
    {
-      String s;
+      String word;
       int mult=1;
+      
+      Bucket(String s)
+      {
+         word = s;
+      }
    }
    
    public static void main(String[] args)
    {
    
       
-      ArrayList<String>[] chain = new ArrayList[1000];
+      ArrayList<Bucket>[] chain = new ArrayList[1000];
       for(int i=0; i<chain.length; i++)
       {
-         chain[i] = new ArrayList<String>();
+         chain[i] = new ArrayList<Bucket>();
       }
       //FileInputStream f = new FileInputStream("constitution.txt");
       Scanner Con = null;
@@ -34,14 +39,23 @@ public class Markov
       //build chain
       while(Con.hasNext())
       {
-         s = Con.next().trim();
+         s = Con.next().trim().toLowerCase(); //remove leading and trailing whitespace
+         if(s.charAt(s.length()-1) == ',' || s.charAt(s.length()-1) == '.')
+            s = s.substring(0, s.length()-1);
          code = Math.abs(s.hashCode());
          try
          {
             
             int i = code % chain.length;
-            if(!s.equals("") && !chain[i].contains(s))
-               chain[i].add(s);
+            int index = -1;
+            if(!s.equals("")) 
+               for(int j=0; j< chain[i].size(); j++)
+                  if(chain[i].get(j).word.equals(s))
+                     index = j;
+            if(index > -1)
+               chain[i].get(index).mult++;
+            else
+               chain[i].add(new Bucket(s));
          }
          catch(Exception e)
          {
@@ -51,6 +65,8 @@ public class Markov
       
       //show chain
       System.out.println("-------Chain data-------");
+      Bucket b=chain[0].get(0);
+      int c = 0;
       for(int i=0; i<chain.length; i++)
       {
          if(chain[i].size() > 0)
@@ -58,12 +74,20 @@ public class Markov
             System.out.println("Bucket " + i+"------");
             for(int j=0; j<chain[i].size(); j++)
             {
-               System.out.print(chain[i].get(j) + ", ");
+               System.out.print(chain[i].get(j).word + " (X"+ chain[i].get(j).mult + "), ");
+               
+               if(b.mult < chain[i].get(j).mult)
+               {
+                  b = chain[i].get(j);
+                  c = i;
+               }
             }
             System.out.println();
          }
          
       }
+      
+      System.out.println("The most used word is: " + b.word + " (" + b.mult + " bucket: " + c + ")");
       
       
    }
